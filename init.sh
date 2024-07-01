8ea60b46e01f1bd4ddc18cb733f709719d0d92ae
jarnaud2 <162626908+jarnaud2@users.noreply.github.com>
2024-07-01 15:18:28 -0400

    Update and rename init_test.sh to init.sh

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script will initialize a new project
+
+echo 'Creating New Project'
+echo ''
+
+mkdir models
+
+module load python
+
+echo 'Creating Virtual Environment'
+echo ''
+python -m venv env
+
+source env/bin/activate
+
+pip install --upgrade pip==21.0
+
+pip install numpy==1.26.4 tensorflow==2.7.0 matplotlib==3.8.4 deepxde==1.10.0
+
