0503d1511af43578a237f82b151103d1db2606a4
jarnaud2 <162626908+jarnaud2@users.noreply.github.com>
2024-07-01 20:19:31 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -17,5 +17,5 @@ source env/bin/activate
 
 pip install --upgrade pip==21.0
 
-pip install numpy==1.26.4 tensorflow==2.7 matplotlib==3.8.0 deepxde==1.10.0
+pip install numpy==1.26.4 tensorflow==2.9.0 matplotlib==3.8.0 deepxde==1.10.0
 
